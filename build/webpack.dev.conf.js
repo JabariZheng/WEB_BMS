6b6b13660f41c59c797c168d918b696182b5b710
JABARI\Doudo <Doudou105@outlook.com>
2018-02-19 16:07:02 +0800

    add favico.ico

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -29,6 +29,7 @@ module.exports = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
+      favicon:'./favico.ico',    // favicon 路径
       inject: true
     }),
     new FriendlyErrorsPlugin()
